c2b3851cbfe39be3121c128d23f7cda1fc5c842c
Rammohan Y <rammohany@virtusa.com>
2014-07-03 17:55:31 +0530

    fixed a bug in thindata.js

--- content/src/main/content/jcr_root/etc/designs/tc/components/tc-newsletter/source/thindata.js
+++ content/src/main/content/jcr_root/etc/designs/tc/components/tc-newsletter/source/thindata.js
@@ -11,10 +11,17 @@ CQ.mcm.utils.Newsletter.thindatasend = function(dialog, box, successCb, failureC
 
     //formUrl = formUrl + "?action=broadcast&mailListName=" + mailList  + "&url=" + curPageUrl
     console.log(formUrl)
-	var d = dialog.items.get(0).items.get(4).items.get(0).getValue();
 
-    var dateStr = d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + d.getDate()  + " " +  d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
-    console.log(dateStr);
+    var dateStr = "";
+    var dateItem = dialog.items.get(0).items.get(4);
+    console.log(dateItem);
+    if (dateItem != null &&  dateItem.get(0).items != null && dateItem.get(0).items.length > 4) {
+		console.log("inside if");
+		var d = dateItem.get(0).getValue();
+        console.log(d);
+    	dateStr = d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + d.getDate()  + " " +  d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+    	console.log(dateStr);
+    }
     var tpl = CQ.mcm.utils.Newsletter.FLIGHT_TEST_RESULT_TMPL;
     var heading = CQ.I18n.getMessage("Result");
 
