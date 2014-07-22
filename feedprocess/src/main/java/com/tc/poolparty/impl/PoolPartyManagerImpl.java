1eb919036a1c7c4f5597c1b65d0438a892a14a93
Rammohan Y <rammohany@virtusa.com>
2014-07-22 14:52:22 +0530

    modified crawltext to get all child tags as well

--- feedprocess/src/main/java/com/tc/poolparty/impl/PoolPartyManagerImpl.java
+++ feedprocess/src/main/java/com/tc/poolparty/impl/PoolPartyManagerImpl.java
@@ -30,7 +30,6 @@ import org.json.JSONObject;
 
 import com.tc.aem.importer.AEMPackageImporter;
 import com.tc.poolparty.PoolPartyManager;
-import com.tc.poolparty.PoolPartyTagsImporter;
 import com.tc.process.handler.TCTransformerHandler;
 
 public class PoolPartyManagerImpl implements PoolPartyManager {
@@ -90,21 +89,21 @@ public class PoolPartyManagerImpl implements PoolPartyManager {
 			}
 		}
 		createParentTagContentXML(tags, destinationFolder, organizationName);
-		
+
 		for (PoolPartyBean tag : tags) {
-				String xmlContent = createXMLContent(tag);
-				createContentXML(tag, xmlContent, destinationFolder);
-				List<PoolPartyBean> childTags = tag.getTags();
-				for (PoolPartyBean bean : childTags) {
-					xmlContent = createXMLContent(bean);
-					createContentXML(bean, xmlContent, destinationFolder + File.separator + tag.getKey());
-					createChildXml(bean, destinationFolder + File.separator + tag.getKey());
-				}
-				
+			String xmlContent = createXMLContent(tag);
+			createContentXML(tag, xmlContent, destinationFolder);
+			List<PoolPartyBean> childTags = tag.getTags();
+			for (PoolPartyBean bean : childTags) {
+				xmlContent = createXMLContent(bean);
+				createContentXML(bean, xmlContent, destinationFolder
+						+ File.separator + tag.getKey());
+				createChildXml(bean,
+						destinationFolder + File.separator + tag.getKey());
+			}
+
 		}
-		
-		
-		
+
 		File jcrRootParentFolder = new File(jcrRootParentDir);
 		FileOutputStream fos = null;
 		ZipOutputStream zos = null;
@@ -145,13 +144,13 @@ public class PoolPartyManagerImpl implements PoolPartyManager {
 		String aemUserName = aemProperties.getProperty("aem.userid");
 		String aemPassword = aemProperties.getProperty("aem.password");
 
-		/*boolean uninstallationStatus = aemPackageImporter.uninstallPackage(
-				repoURL, aemUserName, aemPassword, aemPackagePath);
-		if (uninstallationStatus) {
-			LOG.info("Package uninstalled successfully...");
-		} else {
-			LOG.info("Failed to uninstall the Package...");
-		}*/
+		/*
+		 * boolean uninstallationStatus = aemPackageImporter.uninstallPackage(
+		 * repoURL, aemUserName, aemPassword, aemPackagePath); if
+		 * (uninstallationStatus) {
+		 * LOG.info("Package uninstalled successfully..."); } else {
+		 * LOG.info("Failed to uninstall the Package..."); }
+		 */
 		boolean installationStatus = aemPackageImporter.importPackage(repoURL,
 				aemUserName, aemPassword, aemPackagePath, false);
 		if (installationStatus) {
@@ -160,14 +159,16 @@ public class PoolPartyManagerImpl implements PoolPartyManager {
 			LOG.info("Failed to install the package...");
 		}
 	}
-	
+
 	public void createChildXml(PoolPartyBean parentTag, String parentFolder) {
 
 		if (parentTag.getTags() != null && parentTag.getTags().size() > 0) {
 			for (PoolPartyBean tag : parentTag.getTags()) {
 				String xmlContent = createXMLContent(tag);
-				createContentXML(tag, xmlContent, parentFolder + File.separator + parentTag.getKey());
-				createChildXml(tag, parentFolder + File.separator + parentTag.getKey());
+				createContentXML(tag, xmlContent, parentFolder + File.separator
+						+ parentTag.getKey());
+				createChildXml(tag,
+						parentFolder + File.separator + parentTag.getKey());
 			}
 		}
 	}
@@ -339,7 +340,8 @@ public class PoolPartyManagerImpl implements PoolPartyManager {
 	private String createXMLContent(PoolPartyBean tag) {
 		String xmlContent = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
 				+ "<jcr:root xmlns:sling=\"http://sling.apache.org/jcr/sling/1.0\" xmlns:cq=\"http://www.day.com/jcr/cq/1.0\" xmlns:jcr=\"http://www.jcp.org/jcr/1.0\" jcr:description=\"\" jcr:primaryType=\"cq:Tag\" jcr:title=\""
-				+ tag.getKey() + "\" sling:resourceType=\"cq/tagging/components/tag\"/>";
+				+ tag.getKey()
+				+ "\" sling:resourceType=\"cq/tagging/components/tag\"/>";
 		return xmlContent;
 	}
 
@@ -359,7 +361,8 @@ public class PoolPartyManagerImpl implements PoolPartyManager {
 		createTagFolder(tag, destinationFolder);
 		FileWriter fw = null;
 		try {
-			File tagFolder = new File(destinationFolder + File.separator + tag.getKey());
+			File tagFolder = new File(destinationFolder + File.separator
+					+ tag.getKey());
 			if (tagFolder.exists()) {
 				fw = new java.io.FileWriter(destinationFolder + File.separator
 						+ tag.getKey() + File.separator + ".content.xml");
@@ -387,7 +390,8 @@ public class PoolPartyManagerImpl implements PoolPartyManager {
 	 *            the destination folder
 	 */
 	private void createTagFolder(PoolPartyBean tag, String destinationFolder) {
-		File tagFolder = new File(destinationFolder + File.separator + tag.getKey());
+		File tagFolder = new File(destinationFolder + File.separator
+				+ tag.getKey());
 		if (!tagFolder.exists()) {
 			if (tagFolder.mkdir()) {
 				LOG.info(tagFolder.getName() + " Folder is created!");
@@ -398,12 +402,13 @@ public class PoolPartyManagerImpl implements PoolPartyManager {
 			LOG.info(tagFolder.getName() + " is already exists");
 		}
 	}
+
 	private JSONArray getNarrowers(String uri, boolean schemeFlag, String locale) {
 		String masterJSON = getJSONFromPoolParty(uri, schemeFlag, locale);
 		JSONArray bindings = null;
 		JSONArray array = null;
 		if (!StringUtils.isEmpty(masterJSON)) {
-			
+
 			bindings = new JSONArray(masterJSON);
 			int length = bindings.length();
 			for (int i = 0; i < length; i++) {
@@ -411,54 +416,58 @@ public class PoolPartyManagerImpl implements PoolPartyManager {
 				try {
 					array = binding.getJSONArray("narrowers");
 				} catch (JSONException e) {
-					
+
 				}
-				
+
 			}
-			
+
 		}
-		
+
 		return array;
-		
+
 	}
-	
+
 	private static int indent = 2;
 	/*
-	 * this property is for dev testing, when firstTime is true
-	 * the recursive call will happen only for the first node. 
+	 * this property is for dev testing, when firstTime is true the recursive
+	 * call will happen only for the first node.
 	 */
 	private boolean firstTime = false;
+
 	@Override
-	public List<PoolPartyBean> getTags(String concepts, boolean schemeFlag, String locale) {
-		//LOG.info("Entered getTags()");
-		
+	public List<PoolPartyBean> getTags(String concepts, boolean schemeFlag,
+			String locale) {
+		// LOG.info("Entered getTags()");
+
 		List<PoolPartyBean> tags = null;
 		String masterJSON = getJSONFromPoolParty(concepts, schemeFlag, locale);
 		if (StringUtils.isEmpty(masterJSON)) {
 			return tags;
 		}
-		
+
 		try {
 			JSONArray bindings = new JSONArray(masterJSON);
 			tags = new ArrayList<PoolPartyBean>();
 			LOG.debug(concepts + " has " + bindings.length());
 			/*
-			 * the length variable will be set 1 to test the
-			 * function for only first node. make firstTime flag as false
-			 * to run the recursive function for all tags
+			 * the length variable will be set 1 to test the function for only
+			 * first node. make firstTime flag as false to run the recursive
+			 * function for all tags
 			 */
 			int length = bindings.length();
 			if (firstTime) {
 				length = 2;
 			}
-			
+
 			for (int i = 0; i < length; i++) {
 				JSONArray narrowers = null;
 				JSONObject binding = bindings.getJSONObject(i);
 				String prefLabel = binding.getString("prefLabel");
 				if (prefLabel != null) {
 					/*
-					 * some taxonomy labels contains characters which are not acceptable under a folder name, hence replacing them with valid chars
+					 * some taxonomy labels contains characters which are not
+					 * acceptable under a folder name, hence replacing them with
+					 * valid chars
 					 */
 					prefLabel = prefLabel.replaceAll("/", "_");
 					prefLabel = prefLabel.replaceAll("\\\\", "-");
@@ -475,12 +484,12 @@ public class PoolPartyManagerImpl implements PoolPartyManager {
 					LOG.error(e);
 					charSetPrefLabel = prefLabel;
 				}
-				
+
 				msg.append(charSetPrefLabel);
 				LOG.info(msg.toString());
-				
+
 				PoolPartyBean bean = new PoolPartyBean(charSetPrefLabel);
-				
+
 				List<String> childUris = new ArrayList<String>();
 				try {
 					narrowers = binding.getJSONArray("narrowers");
@@ -488,19 +497,20 @@ public class PoolPartyManagerImpl implements PoolPartyManager {
 					String uri = binding.getString("uri");
 					narrowers = getNarrowers(uri, false, locale);
 				}
-				
+
 				if (narrowers != null) {
 					for (int j = 0; j < narrowers.length(); j++) {
 						String narrower = narrowers.getString(j);
 						childUris.add(narrower);
-						indent +=2;
-						//firstTime = false;
-						List<PoolPartyBean> children = getTags(narrower, false, locale);
-						indent -=2;
+						indent += 2;
+						// firstTime = false;
+						List<PoolPartyBean> children = getTags(narrower, false,
+								locale);
+						indent -= 2;
 						if (children != null) {
-							bean.getTags().addAll(children);	
+							bean.getTags().addAll(children);
 						}
-						
+
 					}
 				}
 				bean.setNarrowers(childUris);
@@ -509,17 +519,17 @@ public class PoolPartyManagerImpl implements PoolPartyManager {
 		} catch (JSONException e) {
 			LOG.error(e);
 		}
-		
+
 		return tags;
 	}
-	
 
 	/**
 	 * Connects to PoolParty. Gets the jSON from pool party.
 	 * 
 	 * @return the jSON from pool party
 	 */
-	private String getJSONFromPoolParty(String topConcepts, boolean schemeFlag, String locale) {
+	private String getJSONFromPoolParty(String topConcepts, boolean schemeFlag,
+			String locale) {
 		StringBuilder content = null;
 		String serverAddress = poolPropertyProperties
 				.getProperty("poolparty.serverAddress");
@@ -557,18 +567,24 @@ public class PoolPartyManagerImpl implements PoolPartyManager {
 		}
 		BufferedReader in = null;
 		try {
-			//String topConcepts = poolPropertyProperties.get("poolparty.topconcepts").toString();
-			String projectId = poolPropertyProperties.get("poolparty.projectId").toString();
+			// String topConcepts =
+			// poolPropertyProperties.get("poolparty.topconcepts").toString();
+			String projectId = poolPropertyProperties
+					.get("poolparty.projectId").toString();
 			URL url = null;
 			if (schemeFlag) {
-				url = new URL(serverAddress + projectId + "/topconcepts?scheme=" + topConcepts+ "&properties=skos:narrower&locale=" + locale);	
+				url = new URL(serverAddress + projectId
+						+ "/topconcepts?scheme=" + topConcepts
+						+ "&properties=skos:narrower&locale=" + locale);
 			} else {
-				url = new URL(serverAddress + projectId + "/concepts?concepts=" + topConcepts + "&properties=skos:narrower&locale=" + locale);
+				url = new URL(serverAddress + projectId + "/concepts?concepts="
+						+ topConcepts + "&properties=skos:narrower&locale="
+						+ locale);
 			}
-			//LOG.info(url);
-			
-			//http://tc.poolparty.biz/api/thesaurus/1DBC909E-6D1C-0001-917C-194CD8B0B2B0/concepts?concepts=http://tc.poolparty.biz/labIPTC/697&properties=skos:narrower&locale
-			HttpURLConnection connection = (HttpURLConnection) url 
+			// LOG.info(url);
+
+			// http://tc.poolparty.biz/api/thesaurus/1DBC909E-6D1C-0001-917C-194CD8B0B2B0/concepts?concepts=http://tc.poolparty.biz/labIPTC/697&properties=skos:narrower&locale
+			HttpURLConnection connection = (HttpURLConnection) url
 					.openConnection();
 			String userpassword = userId + ":" + password;
 			String encoded = "Basic "
@@ -579,18 +595,18 @@ public class PoolPartyManagerImpl implements PoolPartyManager {
 			connection.setRequestProperty("Content-Type", contentType);
 			connection.setConnectTimeout(connectTimeout);
 			connection.setReadTimeout(readTimeout);
-			
-			
-			
-			//String encodedQuery = URLEncoder.encode(query.toString(), "UTF-8");
-			//connection.setRequestProperty("content-type", encodedContentType);
-			//OutputStreamWriter out = new OutputStreamWriter(
-			//		connection.getOutputStream());
-			//out.write("query=" + encodedQuery);
-			//out.write("&format="
-			//		+ URLEncoder.encode("application/json", "utf-8"));
-			//out.flush();
-			//out.close();
+
+			// String encodedQuery = URLEncoder.encode(query.toString(),
+			// "UTF-8");
+			// connection.setRequestProperty("content-type",
+			// encodedContentType);
+			// OutputStreamWriter out = new OutputStreamWriter(
+			// connection.getOutputStream());
+			// out.write("query=" + encodedQuery);
+			// out.write("&format="
+			// + URLEncoder.encode("application/json", "utf-8"));
+			// out.flush();
+			// out.close();
 			in = new BufferedReader(new InputStreamReader(
 					connection.getInputStream()));
 			String line;
@@ -613,7 +629,7 @@ public class PoolPartyManagerImpl implements PoolPartyManager {
 				if (in != null) {
 					in.close();
 				}
-					
+
 			} catch (IOException e) {
 				LOG.error(e);
 			}
@@ -623,16 +639,14 @@ public class PoolPartyManagerImpl implements PoolPartyManager {
 		} else {
 			return "";
 		}
-		
 
 	}
 
-    @Override
-    public List<String> crawlText(String text) {
-        return crawlText(text, poolPropertyProperties
-                .getProperty("poolparty.language"));
-    }
-
+	@Override
+	public List<String> crawlText(String text) {
+		return crawlText(text,
+				poolPropertyProperties.getProperty("poolparty.language"));
+	}
 
 	@Override
 	public List<String> crawlText(String text, String language) {
@@ -647,7 +661,7 @@ public class PoolPartyManagerImpl implements PoolPartyManager {
 
 		LOG.info(urlStr + "--" + user + "--" + projectId + "--" + language);
 		try {
-		
+
 			StringBuffer query = new StringBuffer(String.format("text=%s",
 					URLEncoder.encode(text, charSet)));
 			query.append("&").append(
@@ -689,7 +703,7 @@ public class PoolPartyManagerImpl implements PoolPartyManager {
 			while ((line = in.readLine()) != null) {
 				content.append(line + "\n");
 			}
-			LOG.debug(content.toString());
+			LOG.info(content.toString());
 
 			in.close();
 
@@ -706,22 +720,53 @@ public class PoolPartyManagerImpl implements PoolPartyManager {
 	private List<String> getTagsFromJson(String content) {
 		List<String> tags = new ArrayList<String>();
 		JSONObject root = new JSONObject(content);
+		String organizationName = poolPropertyProperties
+				.getProperty("poolparty.organizationName");
 		try {
 			JSONArray jsonArray = root.getJSONArray("categories");
 			if (jsonArray != null) {
 				for (int i = 0; i < jsonArray.length(); i++) {
 					JSONObject obj = jsonArray.getJSONObject(i);
-					String organizationName = poolPropertyProperties
-							.getProperty("poolparty.organizationName");
-					String tag = organizationName + ":" + obj.getString("prefLabel");
-
+					String tag = organizationName + ":"
+							+ obj.getString("prefLabel");
 					tags.add(tag);
 				}
 			}
+
+			// check for each tag return are there any child tags under
+			// categoryConceptResults
+			for (int i = 0; i < jsonArray.length(); i++) {
+				JSONObject obj = jsonArray.getJSONObject(i);
+				String parentTagName = obj.getString("prefLabel");
+				StringBuffer sb = new StringBuffer();
+				sb.append(organizationName).append(":");
+				try {
+					JSONArray subTags = obj
+							.getJSONArray("categoryConceptResults");
+					for (int j = 0; j < subTags.length(); j++) {
+						JSONObject childTag = subTags.getJSONObject(j);
+						String childTagName = childTag.getString("prefLabel");
+						if (!StringUtils.equalsIgnoreCase(parentTagName,
+								childTagName)) {
+							sb.append(parentTagName).append("/")
+									.append(childTagName);
+						}
+					}
+				} catch (JSONException e) {
+					LOG.info("No sub tags found under categoryConceptResults");
+				}
+				String childTagFull = sb.toString();
+				if (!StringUtils.isEmpty(childTagFull)
+						&& !StringUtils.equalsIgnoreCase(childTagFull,
+								organizationName + ":")) {
+					tags.add(childTagFull);
+					LOG.info("childTags=" + childTagFull);
+				}
+			}
+
 		} catch (JSONException e) {
 			LOG.info("Did not get any categories [tags]");
 		}
-		
 
 		return tags;
 	}
