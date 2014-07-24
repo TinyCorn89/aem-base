5e1ce03bb2c9bc4c8cb517eb3513e1719a863cbd
Adolfo <adolfodepazvela@gmail.com>
2014-07-23 22:49:28 -0400

    clean up includes

--- bundle/service/src/main/java/com/tc/process/handler/TCFTPAdsProcessHandler.java
+++ bundle/service/src/main/java/com/tc/process/handler/TCFTPAdsProcessHandler.java
@@ -1,23 +1,15 @@
 package com.tc.process.handler;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Calendar;
 import java.util.Properties;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import javax.jcr.Binary;
 import javax.jcr.Node;
 import javax.jcr.Session;
 
-import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.net.ftp.FTPFile;
-import org.apache.commons.net.ftp.FTPFileFilter;
 import org.apache.log4j.Logger;
 
 import com.day.cq.commons.jcr.JcrUtil;
